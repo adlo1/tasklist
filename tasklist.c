1847b188ca6445a186201c1e6d14f25375025ce9
adlo1 <adloconwy@gmail.com>
2015-05-08 00:07:59 +0100

    Update tasklist.c

--- tasklist.c
+++ tasklist.c
@@ -38,14 +38,15 @@ GType my_tasklist_get_type (void)
 static void my_tasklist_class_init (MyTasklistClass *klass)
 
 {
-	button_click_action_signals [BUTTON_CLICK_ACTION_SIGNAL] = g_signal_new ("button-click-action",
-																G_TYPE_FROM_CLASS(klass),
-																G_SIGNAL_RUN_FIRST|G_SIGNAL_ACTION,
-																0,
-																NULL,
-																NULL,
-																g_cclosure_marshal_VOID__VOID,
-																G_TYPE_NONE, 0);
+	button_click_action_signals [BUTTON_CLICK_ACTION_SIGNAL] = 
+		g_signal_new ("button-click-action",
+		G_TYPE_FROM_CLASS(klass),
+		G_SIGNAL_RUN_FIRST|G_SIGNAL_ACTION,
+		0,
+		NULL,
+		NULL,
+		g_cclosure_marshal_VOID__VOID,
+		G_TYPE_NONE, 0);
 	
 }
 
@@ -108,72 +109,72 @@ static void my_tasklist_update_windows (MyTasklist *tasklist, gboolean new_windo
       //Determine if window is on the active workspace
       
       if(wnck_window_is_on_workspace(tasklist->window,wnck_screen_get_active_workspace(tasklist->screen)))
-      {
-		  const GtkTargetEntry targets [] = {"application/x-wnck-window-id",0,0};
-		 GtkWidget *label = gtk_label_new_with_mnemonic(wnck_window_get_name (tasklist->window));
-      tasklist->button = gtk_button_new();
-      gtk_label_set_ellipsize(GTK_LABEL(label),PANGO_ELLIPSIZE_END);
-      gtk_container_add (GTK_CONTAINER(tasklist->button),label);
-      gtk_widget_set_size_request (tasklist->button, 200, 40);
-      
-      
-    
-      gulong xid = wnck_window_get_xid (tasklist->window);
-        
-       if (!(wnck_window_is_skip_tasklist(tasklist->window)))
-       
-       {
-        
-      gtk_table_attach_defaults (GTK_TABLE(tasklist->table), tasklist->button, left_attach, 
-		right_attach, top_attach, bottom_attach);
-	  
-	  
-	  
-	  g_signal_connect (tasklist->button, "clicked", my_tasklist_button_clicked, tasklist->window);
-    g_signal_connect (GTK_BUTTON(tasklist->button), "clicked", my_tasklist_button_emit_signal, tasklist);
-    
-    if (right_attach % 3 == 0)
-    {
-		top_attach++;
-		bottom_attach++;
-		left_attach=0;
-		right_attach=1;
-	}
-	else
 	{
-		left_attach++;
-		right_attach++;
-	}
+		const GtkTargetEntry targets [] = {"application/x-wnck-window-id",0,0};
+		GtkWidget *label = gtk_label_new_with_mnemonic(wnck_window_get_name (tasklist->window));
+		tasklist->button = gtk_button_new();
+		gtk_label_set_ellipsize(GTK_LABEL(label),PANGO_ELLIPSIZE_END);
+		gtk_container_add (GTK_CONTAINER(tasklist->button),label);
+		gtk_widget_set_size_request (tasklist->button, 200, 40);
+      
+      
+    
+		gulong xid = wnck_window_get_xid (tasklist->window);
+        
+		if (!(wnck_window_is_skip_tasklist(tasklist->window)))
+       
+		{
+        
+			gtk_table_attach_defaults (GTK_TABLE(tasklist->table), tasklist->button, left_attach, 
+					right_attach, top_attach, bottom_attach);
+	  
+	  
+	  
+			g_signal_connect (tasklist->button, "clicked", my_tasklist_button_clicked, tasklist->window);
+			g_signal_connect (GTK_BUTTON(tasklist->button), "clicked", my_tasklist_button_emit_signal, tasklist);
+    
+			if (right_attach % 3 == 0)
+			{
+				top_attach++;
+				bottom_attach++;
+				left_attach=0;
+				right_attach=1;
+			}
+			else
+			{
+				left_attach++;
+				right_attach++;
+			}
 	
-	gtk_drag_source_set (tasklist->button,GDK_BUTTON1_MASK,targets,1,GDK_ACTION_MOVE);
+			gtk_drag_source_set (tasklist->button,GDK_BUTTON1_MASK,targets,1,GDK_ACTION_MOVE);
 	
-	g_signal_connect (tasklist->button, "drag-data-get",
-                G_CALLBACK (my_tasklist_drag_data_get_handl), xid);
+			g_signal_connect (tasklist->button, "drag-data-get",
+					G_CALLBACK (my_tasklist_drag_data_get_handl), xid);
             
-  g_signal_handlers_disconnect_matched (tasklist->window, G_SIGNAL_MATCH_FUNC, 
-	NULL, NULL, NULL, my_tasklist_on_name_changed, NULL);
+			g_signal_handlers_disconnect_matched (tasklist->window, G_SIGNAL_MATCH_FUNC, 
+					NULL, NULL, NULL, my_tasklist_on_name_changed, NULL);
   
 
 	
-	g_signal_connect (tasklist->window, "name-changed",
-    G_CALLBACK (my_tasklist_on_name_changed), label);
+			g_signal_connect (tasklist->window, "name-changed",
+					G_CALLBACK (my_tasklist_on_name_changed), label);
     
     
     
-    //If the window is new, connect to its workspace-changed signal
+			//If the window is new, connect to its workspace-changed signal
                 
-	if (new_window)
+			if (new_window)
                 
             {                                           
-                g_signal_handlers_disconnect_by_func (tasklist->window, my_tasklist_window_workspace_changed, tasklist);
+				g_signal_handlers_disconnect_by_func (tasklist->window, my_tasklist_window_workspace_changed, tasklist);
                 
                 g_signal_connect (tasklist->window, "workspace-changed",
-                G_CALLBACK (my_tasklist_window_workspace_changed), tasklist);                
+					G_CALLBACK (my_tasklist_window_workspace_changed), tasklist);                
 			}
                 
     
-}
-}
+		}
+	}
 }
 
 gtk_container_add(GTK_CONTAINER(tasklist), tasklist->table);
